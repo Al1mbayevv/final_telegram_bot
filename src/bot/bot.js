7a3f4e0cb5ee662d8a10cde5ceca435abf681151
salom <7alimbayevv@gmail.com>
2025-12-06 12:47:39 +0500

    e

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,19 +1,28 @@
 import TelegramBot from "node-telegram-bot-api";
 import { config } from "dotenv";
+import onStart from "./handlers/onStart.js";
+import onProfile from "./handlers/onProfile.js"
 config()
 
 const TOKEN = process.env.BOT_TOKEN;
 
-const bot = new TelegramBot(TOKEN, { polling: true });
+export const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.on("message", function (msg) {
-    const chatIt = msg.chat.id;
+    const chatId = msg.chat.id;
     const firstname = msg.chat.first_name;
     const text = msg.text;
 
-    bot.sendMessage(chatIt, `Assalomu aleykum, ${firstname}`);
-    bot.sendMessage(chatIt, `${text}`);
+    if (text == "/start") {
+        return onStart(msg)
+    }
 
+    if (text == "/profile") {
+        return onProfile(msg)
+    }
+
+
+    return bot.sendMessage(chatId, `Kutilmagan xatolik...! /start bosing iltimos!`);
 
 });
 
